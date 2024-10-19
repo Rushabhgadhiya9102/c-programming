303d313870895efb2cdb878aa7632b7907c6a761
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-10-19 23:14:16 +0530

    done

--- project-9(supervision)/que-1.c
+++ project-9(supervision)/que-1.c
@@ -2,31 +2,32 @@
 
 int main (){
 	
-	int i;
-	FILE *oddfile, *evenfile;
+	int i;		// DECLARE THE VARIABLE
+
+	FILE *oddfile, *evenfile;		// FILE POINTER DECLARE
 	
-	oddfile = fopen("oddfile.txt","w");
+	oddfile = fopen("oddfile.txt","w");		// ODD FILE OPEN
 	
 	if(oddfile == NULL){
-		printf("the file is empty or does not exist");
+		printf("the file is empty or does not exist");		// CHECK THE CONDITION OF ODD FILE
 	}
 	
-	evenfile = fopen("evenfile.txt","w");
+	evenfile = fopen("evenfile.txt","w");		// EVEN FILE OPEN
 	
 	if(evenfile == NULL){
-		printf("the file is empty or does not exist");
+		printf("the file is empty or does not exist");		// CHECK THE CONDITION OF EVEN FILE
 	}
 	
-	for(i=50; i<=70; i++){
+	for(i=50; i<=70; i++){		// FOR LOOP START FROM 50 TO 70
 		
 		if(i%2 == 0){
-			fprintf(evenfile,"%d\n",i);
+			fprintf(evenfile,"%d\n",i);		// PRINT EVEN NUMBER
 		}else{
-			fprintf(oddfile,"%d\n",i);
+			fprintf(oddfile,"%d\n",i);		// PRINT ODD NUMBER
 		}
 	}
 	
-	printf("the file is successfully created");
+	printf("the file is successfully created");		// CONFIRM MESSAGE PRINT
 	
 	return 0;
 }
--- project-9(supervision)/que-2.c
+++ project-9(supervision)/que-2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-struct students{
+struct students{		// STRUCTURE IS CREATED
 	
 	int roll_no;
 	char name[50];
@@ -12,47 +12,49 @@ struct students{
 
 int main (){
 	
-	int i,total_marks;
-	struct students s1;
+	int i,total_marks;		// DECALRE THE VARIABLE
+	struct students s1[5];		// CALLING THE STRUCTURE S1
+
+	// ------------------------ FIRST LOOP FOR INPUT ------------------------
 	 
-	for (i=0; i<5; i++){
+	for (i=0; i<5; i++){	
 		
 		printf("\n\nenter the student detail (%d)\n",i+1);
 		printf("Roll_no: ");
-		scanf("%d",&s1.roll_no);
+		scanf("%d",&s1[i].roll_no);		// ENTER THE ROLL NO
 		
 		printf("Name: ");
-		scanf("%s",&s1.name[i]);
+		scanf("%s",&s1[i].name);		// ENTER THE NAME
 		
 		printf("Chemistry: ");
-		scanf("%d",&s1.chemistry);
+		scanf("%d",&s1[i].chemistry);		// ENTER THE SUBJECT MARKS
 		
 		printf("Maths: ");
-		scanf("%d",&s1.maths);
+		scanf("%d",&s1[i].maths);		// ENTER THE SUBJECT MARKS
 		
 		printf("Physics: ");
-		scanf("%d",&s1.physics);
+		scanf("%d",&s1[i].physics);		// ENTER THE SUBJECT MARKS
 	}
 	
 	printf("\n\nOutput\n\n");
+
+	// ------------------------ SECOND LOOP FOR OUTPUT ------------------------
 	
 	for(i=0; i<5; i++){
 		
-		printf("%s (%d)\n",s1.name[i], s1.roll_no);
+		printf("%s (%d)\n",s1[i].name, s1[i].roll_no);		// PRINT NAME AND ROLL NO.
 		
-		printf("Chemistry: %d\n", s1.chemistry);
+		printf("Chemistry: %d\n", s1[i].chemistry);			// PRINT SUBJECT MARKS
 		
-		printf("Maths: %d\n", s1.maths);
+		printf("Maths: %d\n", s1[i].maths);					// PRINT SUBJECT MARKS
 		
-		printf("Physics: %d\n", s1.physics);
+		printf("Physics: %d\n", s1[i].physics);				// PRINT SUBJECT MARKS
 		
-		total_marks = s1.chemistry + s1.maths + s1.physics;
+		total_marks = s1[i].chemistry + s1[i].maths + s1[i].physics;		// TOTAL OF SUBJECT MARKS
 		
-		printf("Total = %d/300\n",total_marks);
-		
-		float percentage = total_marks/300*100;
-		
-		printf("Percentage = %.2f\n\n",percentage);
+		printf("Total: %d/300\n",total_marks); 		// PRINT THE TOTAL
+			
+		printf("Percentage: %.2f%%\n\n",(float)total_marks/300*100);		// PRINT THE PERCENTAGE
 	}
 	
 	return 0;
